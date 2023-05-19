2a650f60f2504858298affb73ec34031e967df21
LiamDes <deshaiesash@gmail.com>
2023-05-19 14:54:49 -0700

    add assets, add checkout details on complete page, create primary banner details on landing

--- stickerfellas/complete/views.py
+++ stickerfellas/complete/views.py
@@ -12,6 +12,5 @@ stripe.api_key = 'sk_test_51N9EumJgrzFtfn7GNSxzdC7QO5dPdtMxsIq3CYtmJk6KX67JaN54e
 # Create your views here.
 @api_view(['GET'])
 def order_success(request):
-    session = stripe.checkout.Session.retrieve(request.GET.get('session_id'))
-    # return Response({'info': session})
+    session = stripe.checkout.Session.retrieve(request.GET.get('session_id'), expand=['line_items'])
     return render(request, 'order_complete.html', {'info': session})
